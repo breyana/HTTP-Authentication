c8c918281693386a061747db8c580b0f3763ba73
Breyana Scales <breyanaj@gmail.com>
2017-08-17 13:16:06 -0700

    Add retrieveUser in db queries

--- db/index.js
+++ db/index.js
@@ -11,6 +11,11 @@ const addUser = (email, password) => {
     })
 }
 
+const retrieveUser = (email, password) => {
+  return db.one('SELECT * FROM users WHERE email = $1 AND password = $2', [email, password])
+}
+
 module.exports = {
-  addUser
+  addUser,
+  retrieveUser
 }
